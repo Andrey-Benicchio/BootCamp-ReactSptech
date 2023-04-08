a5a13ad0737acfe69f25f41b9c2df7efe2be2f25
Benicchioo <100138851+Andrey-Benicchio@users.noreply.github.com>
2023-04-08 13:06:36 -0300

    Add files via upload

---
+++ bootcamp-React/App.js
@@ -0,0 +1,8 @@
+import Musicas from "./pages/Musicas";
+import "./html-css-template/css/style.css"
+import "./html-css-template/css/reset.css"
+function App() {
+  return <Musicas />;
+}
+
+export default App;
--- bootcamp-React/index.js
+++ bootcamp-React/index.js
@@ -1,30 +1,10 @@
-var container = document.getElementById("root");
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-function Participante(props){
-    const[numero, setNumero] = React.useState(0);
-    function votar(){
-       setNumero(numero + 1 );
-
-    }
-    
-    return(
-        <>
-        <h1>{props.nome}</h1>
-        <h1>{numero}</h1>
-        <button onClick={votar}>Votar</button>
-        </>
-    )
-}
-
-function Votacao(){
-    return(
-        <>
-        < Participante nome = "Mac Donald" />
-        < Participante nome = "Bk" />
-        < Participante nome = "Popeys" />
-        </>
-    );
-}
-        
-
-ReactDOM.createRoot(container).render(<Votacao/>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
